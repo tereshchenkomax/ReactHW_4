5126f658b04fe7fa96fe64d1fe44fbbb111f4a9f
Max <tereshchenkomax@gmail.com>
2019-01-09 15:22:36 +0200

    implementation completed
    implementation completed

--- src/SelectForm.js
+++ src/SelectForm.js
@@ -2,20 +2,54 @@ import React, { Component } from "react";
 
 class SelectForm extends Component {
   static formOptions = ["apple", "cherry", "grape", "orange", "pear", "peach"];
-
   state = { multiline: "", commaSeparated: "", multiSelect: [] };
 
-  handleCommaSeparateChange = e => {
-    console.log("handleCommaSeparateChange");
+  handleCommaSeparateChange = ({ target }) => {
+    let opt = SelectForm.formOptions;
+    let match = target.value.split(",").filter(item => {
+      return opt.indexOf(item) > -1;
+    });
+    this.setState({
+      commaSeparated: target.value,
+      multiline: match.map(i => (i = i + "\n")).join(""),
+      multiSelect: match
+    });
   };
-  handleMiltilineChange = e => {
-    console.log("handleMultilineChange");
+
+  handleMiltilineChange = ({ target }) => {
+    let opt = SelectForm.formOptions;
+    let match = target.value.split("\n").filter(item => {
+      return opt.indexOf(item) > -1;
+    });
+
+    this.setState({
+      multiline: target.value,
+      commaSeparated: match,
+      multiSelect: match
+    });
   };
-  handleMiltiselectChange = e => {
-    console.log("handleMultiselectChange");
+
+  handleMiltiselectChange = ({ target }) => {
+    let { multiSelect } = this.state;
+    if (multiSelect.indexOf(target.value) > -1) {
+      this.setState({
+        multiSelect: multiSelect.filter(item => item !== target.value),
+        multiline: multiSelect.filter(item => item !== target.value),
+        commaSeparated: multiSelect.filter(item => item !== target.value)
+      });
+    } else {
+      this.setState({
+        multiSelect: [...multiSelect, target.value],
+        multiline: [...multiSelect, target.value]
+          .map(i => (i = i + "\n"))
+          .join(""),
+        commaSeparated: [...multiSelect, target.value]
+      });
+    }
   };
 
   render() {
+    let { multiline, commaSeparated, multiSelect } = this.state;
     return (
       <div className="container">
         <form>
@@ -25,6 +59,7 @@ class SelectForm extends Component {
               onChange={this.handleCommaSeparateChange}
               type="text"
               className="form-control"
+              value={commaSeparated}
             />
           </div>
 
@@ -34,6 +69,7 @@ class SelectForm extends Component {
               className="form-control"
               rows={SelectForm.formOptions.length}
               onChange={this.handleMiltilineChange}
+              value={multiline}
             />
           </div>
           <div>
@@ -41,6 +77,7 @@ class SelectForm extends Component {
               multiple
               size={SelectForm.formOptions.length}
               onChange={this.handleMiltiselectChange}
+              value={multiSelect}
             >
               {SelectForm.formOptions.map((el, i) => (
                 <option value={el} key={i}>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SelectForm from "./SelectForm";
 
 import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <SelectForm />;
 }
 
 const rootElement = document.getElementById("root");
